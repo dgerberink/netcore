4f42ce2edabfebb3ec29d522731e30e41e9663b4
Donny Gerberink <donny.gerberink@kpnmail.nl>
2020-06-30 22:44:25 +0200

    commit on master

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace netcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Gerberink!");
         }
     }
 }
